ad3939a0d9229f95b966c79f307a03ace0e2d86c
Xemiru <xemiruk@gmail.com>
2017-01-07 00:28:30 +0000

    Add our travis stuff.

---
+++ compile.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -ev
+
+SOURCE="master"
+TARGET="gh-pages"
+TAG="latest"
+
+./gradlew check
+
+# dont do extra if we're not master or if its a pr
+if [[ "$TRAVIS_BRANCH" != "$SOURCE" || "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+	echo "Did not push to $SOURCE or is pull request; building only."
+	exit 0
+fi
+
+if [[ -n "$TRAVIS_TAG" ]]; then
+	TAG="$TRAVIS_TAG"
+fi
+
+bash ./javadocs.sh "$TAG"
---
+++ javadocs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -ev
+
+# get the version we're using, passed to us as a script arg
+VERSION="$1"
+
+# get our deploy key ready
+openssl aes-256-cbc -K $encrypted_1ebe7c3c7313_key -iv $encrypted_1ebe7c3c7313_iv -in gh_pages.enc -out gh_pages -d
+chmod 600 gh_pages
+eval `ssh-agent -s`
+ssh-add gh_pages
+
+# clone our gh-pages branch
+cd "$TRAVIS_BUILD_DIR/../"
+git clone -b gh-pages -- https://github.com/Xemiru/LuaMesh.git jdocs
+cd jdocs
+
+# get into our version directory and clean it out
+rm -rf ./$VERSION
+mkdir $VERSION
+cd $VERSION
+
+# generate jdocs
+CACHEDIR="$HOME/.gradle/caches/modules-2/files-2.1"
+javadoc -sourcepath "$TRAVIS_BUILD_DIR/src/main/java/" -classpath "$CACHEDIR/org.luaj/luaj-jse/3.0.1/99245b2df284805e1cb835e9be47c243f9717511/luaj-jse-3.0.1.jar" -subpackages com.github.xemiru.luamesh -d ./
+
+# push it to the repo
+COMMIT=`git rev-parse --verify HEAD`
+
+git config --global user.name "Travis CI"
+git config --global user.email "xemiruk@gmail.com"
+git add -A
+git commit -m "Update javadocs for version $VERSION: $COMMIT"
+git push git@github.com:Xemiru/LuaMesh.git gh-pages
